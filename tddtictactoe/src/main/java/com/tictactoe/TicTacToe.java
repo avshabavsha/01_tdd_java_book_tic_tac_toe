6c035d9d0120d68482ae4ad999f967747652b2ae
avshaloms <avshalom.schwartz@nice.com>
2018-02-13 10:02:38 +0200

    add second test

--- tddtictactoe/src/main/java/com/tictactoe/TicTacToe.java
+++ tddtictactoe/src/main/java/com/tictactoe/TicTacToe.java
@@ -4,7 +4,11 @@ package com.tictactoe;
  * Created by avshaloms on 13/02/2018.
  */
 public class TicTacToe {
-    public void play(int i, int i1) {
-        throw new RuntimeException();
+    public void play(int x, int y) {
+        if(x < 1 || x > 3)
+            throw new RuntimeException("X outside board");
+
+        if(y < 1 || y > 3)
+            throw new RuntimeException("Y outside board");
     }
 }
--- tddtictactoe/src/test/java/com/tictactoe/TicTacToeSpec.java
+++ tddtictactoe/src/test/java/com/tictactoe/TicTacToeSpec.java
@@ -25,4 +25,10 @@ public class TicTacToeSpec {
         ticTacToe.play(5,2);
     }
 
+    @Test
+    public void whenYOutsideBoardThenRuntimeException(){
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2,5);
+    }
+
 }
